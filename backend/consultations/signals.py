5547924792fc18f2cbc75938090efe971e771d88
minababo <minada965@gmail.com>
2025-05-13 18:55:36 +0530

    Fixed file viewing in admin panel

--- backend/consultations/signals.py
+++ backend/consultations/signals.py
@@ -6,30 +6,6 @@ from users.models import Notification
 @receiver(post_save, sender=ConsultationRequest)
 def create_consultation_notification(sender, instance, created, **kwargs):
     if created:
-        return
-
-    try:
-        previous = ConsultationRequest.objects.get(pk=instance.pk)
-    except ConsultationRequest.DoesNotExist:
-        return
-
-    if previous.status != instance.status:
-        if instance.status == 'accepted':
-            Notification.objects.create(
-                user=instance.client,
-                message=f"Your consultation with {instance.lawyer.lawyerprofile.full_name} was accepted.",
-                status='success'
-            )
-        elif instance.status == 'rejected':
-            Notification.objects.create(
-                user=instance.client,
-                message=f"Your consultation with {instance.lawyer.lawyerprofile.full_name} was rejected.",
-                status='danger'
-            )
-
-@receiver(post_save, sender=ConsultationRequest)
-def create_consultation_notification(sender, instance, created, **kwargs):
-    if created:
         Notification.objects.create(
             user=instance.lawyer,
             message=f"New consultation request from {instance.client.clientprofile.full_name}.",
